5bb0118a7668dad5e3a3e13486bd343ee46003bc
Kevin <kevinhenleyinfo@gmail.com>
2025-08-02 17:50:26 -0500

    quick test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20'>
       <main className='row-start-2 flex flex-col items-center gap-[32px] sm:items-start'>
-        <h1 className='text-8xl font-bold text-orange-400 uppercase italic text-shadow-lg text-shadow-orange-300'>
+        <h1 className='text-8xl font-bold text-purple-400 uppercase italic text-shadow-lg text-shadow-lime-300'>
           The Library App
         </h1>
       </main>
