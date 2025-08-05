8e6f30448b22dea5c5fd0212ff72fd28a00edf2e
Kevin <kevinhenleyinfo@gmail.com>
2025-08-04 22:18:11 -0500

    updated Manifest

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,26 @@
-self.addEventListener('install', () => {
-  console.log('Service worker installed')
+const CACHE_NAME = 'book-library-v1'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll([
+        '/',
+        '/offline.html',
+        '/favicon.ico',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+        '/_next/static/...', // Add relevant assets if needed
+      ]),
+    ),
+  )
 })
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request))
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        return response || caches.match('/offline.html')
+      })
+    }),
+  )
 })
