7562ea4c310c37dc9e5d1642da039af181356c61
Kevin <kevinhenleyinfo@gmail.com>
2025-08-04 23:25:41 -0500

    added /my-books/wishlist to cached pages

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,7 @@ self.addEventListener('install', (event) => {
 
   const urlsToCache = [
     '/',
+    '/my-books/wishlist',
     '/offline.html',
     '/favicon.svg',
     '/icons/icon-128x128.png',
