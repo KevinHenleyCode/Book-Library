8c44679810d74d89fe1b094603b9715cfbfc12bb
Kevin <kevinhenleyinfo@gmail.com>
2025-08-06 08:44:57 -0500

    added Linting rules to enforce comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:jsdoc/recommended',
+    'plugin:jsx-a11y/recommended',
+  ),
+  {
+    plugins: {
+      'jsx-a11y': eslintPluginJsxA11y,
+    },
+    rules: {
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+        },
+      ],
+      'jsdoc/require-description': 'warn',
 
-export default eslintConfig;
+      // Accessibility Rules
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/aria-role': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+    },
+  },
+]
+
+export default eslintConfig
